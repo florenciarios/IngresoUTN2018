4f6976a18115cfc65565692c8bba56d6db5aee9a
Florencia <36230711+florenciarios@users.noreply.github.com>
2018-02-14 12:29:18 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,21 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var nota;
+
+	nota = Math.floor(Math.random() * 10) + 1;
+
+	if (nota >= 9)
+	{
+		alert("Nota: " + nota + ". EXCELENTE");
+	}
+	else if (nota <4)
+	{
+		alert("Nota: " + nota + ". Vamos, la próxima se puede");
+	}
+	else 
+	{
+		alert("Nota: " + nota + ". APROBÓ");
+	}
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,12 @@
 function Mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var numeroAleatorio;
+
+	numeroAleatorio = Math.floor(Math.random() * 10)+1;
+
+	//Muestro número RANDOM
 	
+	alert(numeroAleatorio);
 
 }//FIN DE LA FUNCIÓN
