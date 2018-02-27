6060081c2d274aa3a699ec83a80f05f6277762f9
Florencia <36230711+florenciarios@users.noreply.github.com>
2018-02-27 17:42:36 -0300

    Add files via upload

--- 6-InstruccionFor/jsIteracionesFor (6).js
+++ 6-InstruccionFor/jsIteracionesFor (6).js
@@ -4,13 +4,24 @@ function Mostrar()
 
 for(;;)
 {
-    var num = prompt("Ingrese un numero")
+    var num = prompt("Ingrese un numero");
+    var numPar = 0;
+    var pregunta
 
     if(num %2 == 0)
     {
-
+        numPar++;
     }
+
+    pregunta = prompt("Quiere ingresar otro?")
+    if(pregunta == "no")
+    {
+        break;
+    }
+
+    
 }
 
+alert("La cantidad de números pares encontrados son " + numPar + " y los números son " + num);
 
 }//FIN DE LA FUNCIÓN
